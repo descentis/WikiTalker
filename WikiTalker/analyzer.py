cedf20fb588260b377f997c98fef2807493ff0e4
descentis <mt4descentis@gmail.com>
2020-08-11 16:29:46 +0530

    added the function for downloading Wikipedia Talk pages

--- WikiTalker/analyzer.py
+++ WikiTalker/analyzer.py
@@ -4,3 +4,9 @@ import operator
 import math
 import matplotlib.pyplot as plt
 import requests
+
+def download_file(filename):
+	file = filename
+	url = 'https://wikitalkpages.s3.ap-south-1.amazonaws.com/' + file +'.json'
+	r = requests.get(url, allow_redirects=True)
+	open(file + '.json', 'wb').write(r.content)
