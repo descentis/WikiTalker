383988eec3005cbd0c0c80036be8d129bbf7fc42
descentis <mt4descentis@gmail.com>
2020-08-11 16:42:16 +0530

    added the function for dumpung the dataset in the mongoDB database

--- WikiTalker/analyzer.py
+++ WikiTalker/analyzer.py
@@ -10,3 +10,14 @@ def download_file(filename):
 	url = 'https://wikitalkpages.s3.ap-south-1.amazonaws.com/' + file +'.json'
 	r = requests.get(url, allow_redirects=True)
 	open(file + '.json', 'wb').write(r.content)
+
+
+def putInDatabase(collection_name, file_name, myclient, mongoClientDB):
+	collection = mongoClientDB[collection_name]
+	json_file = open(file_name)
+	data = json_file.read().strip("[]").split("\"},")
+	for i in range(len(data)):
+		if i != len(data)-1:
+			data[i] += "\"}"
+		ins = json.loads(data[i])
+		collection.insert(ins)
