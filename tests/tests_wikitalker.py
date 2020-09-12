0438edcb067017ea01f49904c95050516afaa38e
descentis <mt4descentis@gmail.com>
2020-09-12 14:35:27 +0530

    added the method to extract data from all_talk_pages and dump it into MongoDB

--- tests/tests_wikitalker.py
+++ tests/tests_wikitalker.py
@@ -28,12 +28,42 @@ with open('/media/descentis/0FC10BE60FC10BE6/WikipediaData/CurrentRevision/all_t
         if count == 100:
             break
 '''
+cwd = os.getcwd()
 
-
-shutil.move('sample_file50.xml', '/home/descentis/research/working_packages/witpy/grawitas_extension/sample_file50.xml')
+shutil.move('sample_file20.xml', '/home/descentis/research/working_packages/witpy/grawitas_extension/sample_file20.xml')
 
 os.chdir('/home/descentis/research/working_packages/witpy/grawitas_extension')
-subprocess.call(shlex.split('./controller.sh sample_file50.xml sample.json'))
+subprocess.call(shlex.split('./controller.sh sample_file20.xml sample.json'))
+subprocess.call(shlex.split('rm sample_file20.xml'))
+
+start = 0
+with open('sample.json','r') as myFile:
+    for line in myFile:
+        with open('/home/descentis/research/working_datasets/wikipedia_current/talk_pages_json.txt', 'a') as newFile:
+            start = newFile.tell()
+            newFile.write(line+'\n')
+with open('file_id.txt', 'a') as myFile:
+    myFile.write(str(start)+'\n')
+
+subprocess.call(shlex.split('rm sample.json'))
+
+os.chdir(cwd)
+shutil.move('sample_file88.xml', '/home/descentis/research/working_packages/witpy/grawitas_extension/sample_file88.xml')
+
+os.chdir('/home/descentis/research/working_packages/witpy/grawitas_extension')
+subprocess.call(shlex.split('./controller.sh sample_file88.xml sample.json'))
+subprocess.call(shlex.split('rm sample_file88.xml'))
+
+start = 0
+with open('sample.json','r') as myFile:
+    for line in myFile:
+        with open('/home/descentis/research/working_datasets/wikipedia_current/talk_pages_json.txt', 'a') as newFile:
+            start = newFile.tell()
+            newFile.write(line+'\n')
+with open('file_id.txt', 'a') as myFile:
+    myFile.write(str(start)+'\n')
+
+subprocess.call(shlex.split('rm sample.json'))
 
 '''
 file_list = glob.glob('*.xml')
